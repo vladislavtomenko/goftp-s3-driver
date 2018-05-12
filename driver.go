86e88612ad0ed83087035c6690f6e8936880b19d
Vladislav Tomenko <vladislavtomenko@gmail.com>
2018-05-12 19:19:41 +0300

    feat(driver.go): add import section

--- driver.go
+++ driver.go
@@ -1,1 +1,16 @@
 package s3driver
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/goftp/server"
+)
